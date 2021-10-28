8406438e5b4b079c2cc83a97451e3b4e104b6083
Filip Izydorczyk <filip.izydorczyk@protonmail.com>
2021-10-28 18:17:31 +0200

    added socials

--- src/storage.ts
+++ src/storage.ts
@@ -4,7 +4,18 @@ import { LoremIpsum } from "lorem-ipsum";
 export const email: string = "filip.izydorczyk@protonmail.com";
 export const name: string = "Filip Izydorczyk";
 
-export const socials: Array<Social> = [];
+export const socials: Array<Social> = [
+    {
+        name: "GitHub",
+        href: "https://github.com/filipizydorczyk/",
+        icon: "/assets/icons/github.svg",
+    },
+    {
+        name: "Linkedin",
+        href: "https://www.linkedin.com/in/filip-izydorczyk-39577a216/",
+        icon: "/assets/icons/linked_in.svg",
+    },
+];
 
 export const loremIpsum = new LoremIpsum({
     sentencesPerParagraph: {
